ac83ca355d3b540aa30024ac4d9b5887ee6ece78
Phil <xuyuwei19940909@gmail.com>
2024-11-27 11:49:58 -0800

    done

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 $(function() {
-  // your code here
+  $('.accordion-header').on('click', function(e) {
+    const content = $(this).next('.accordion-content')
+
+    if (content.is(':visible')) {
+      content.slideUp()
+    } else {
+      content.slideDown()
+    }
+  })
+
+  $('.todos').find('button').on('click', function() {
+    const ul = $(this).next();
+
+    ul.empty();
+
+    $.ajax({
+      url: 'https://dummyjson.com/todos',
+      type: 'GET',
+      success: function(data) {
+        let innerHtml = '';
+
+        data.todos.forEach(function(item) {
+          innerHtml += `<li>${item.todo}</li>`;
+        })
+
+        ul.html(innerHtml)
+      },
+      error: function(error) {
+        console.log(error)
+      }
+    })
+  })
 })
